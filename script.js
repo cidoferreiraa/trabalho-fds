dd5f6b1afb454d2814a80bc98e6f13655a19fb40
cidoferreiraa <ferreiracido36@gmail.com>
2025-04-23 02:36:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let botao= document.querySelector("#botao")
+botao.style.background="yellow"
+botao.addEventListener("click" ,mudatexto)
+
+function mudatexto(){
+    botao.innerHTML="Se vai sonhar,exagera!"
+
+}
